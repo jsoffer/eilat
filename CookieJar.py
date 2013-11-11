0e0affe1328262f382e5ac140a50e4d4fe325668
Jaime Soffer <jaime.soffer@gmail.com>
2013-11-11 04:21:49 -0600

    logs the setting and reading of cookies

--- CookieJar.py
+++ CookieJar.py
@@ -2,5 +2,10 @@ from PyQt4.QtNetwork import QNetworkCookieJar
 
 class CookieJar(QNetworkCookieJar):
     def setCookiesFromUrl(self,cookies,url):
-        print "COOKIES " + url.host() + ": " + ", ".join(map(lambda k: "["+unicode(k.domain())+"]" + unicode(k.name()) + " => " + unicode(k.value()), cookies))
+        print "< COOKIES " + url.host() + url.path() + ": " + ", ".join(map(lambda k: "["+unicode(k.domain())+unicode(k.path())+"]" + unicode(k.name()) + " => " + unicode(k.value()), cookies))
         return QNetworkCookieJar.setCookiesFromUrl(self,cookies,url)
+    def cookiesForUrl(self,url):
+        ret = QNetworkCookieJar.cookiesForUrl(self,url)
+        if ret:
+            print "> COOKIES " + url.host() + url.path() + ": " + ", ".join(map(lambda k: "["+unicode(k.domain())+unicode(k.path())+"]" + unicode(k.name()) + " => " + unicode(k.value()), ret))
+        return ret
