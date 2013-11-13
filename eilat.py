8b8de80070e8b847a93e7f8d0fd42de7304c54e3
Jaime Soffer <jaime.soffer@gmail.com>
2013-11-13 13:08:50 -0600

    version bump

--- eilat.py
+++ eilat.py
@@ -114,7 +114,7 @@ if __name__ == "__main__":
   netmanager.setCookieJar(cookiejar)
 
   app.setApplicationName("Eilat")
-  app.setApplicationVersion("0.001")
+  app.setApplicationVersion("1.001")
   mainwin = MainWin(netmanager, cb)
   mainwin.show()
 
