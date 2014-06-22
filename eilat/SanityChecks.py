126ffa83b6f299a94cd99e7e7d02a78dc6c160db
Jaime Soffer <jaime.soffer@gmail.com>
2014-06-22 12:02:59 -0500

    check for PyQt4 before anything else

--- eilat/SanityChecks.py
+++ eilat/SanityChecks.py
@@ -1,6 +1,26 @@
+""" Checks to be performed before the application starts """
+
+
 def check_libraries():
+    """ Are all required modules available? """
+
     try:
         import PyQt4
     except ImportError:
-        print("PyQt4 is not available")
+        print("""
+
+        ----
+        PyQt4 is not available
+
+        It's not automatically installed through pip because I (the packager)
+        can't get beyond 'No distributions at all found for PyQt4' after 'pip
+        install PyQt4'
+
+        Please install either from your package manager or by following the
+        instructions on the wiki, https://github.com/jsoffer/eilat/wiki/set-up
+        ----
+
+
+        """)
+
         raise
